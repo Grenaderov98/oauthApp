594bfe61e0f4f160b794f9f1cdfe5917c76179a7
Sergei Grenaderov <grenaderov2020@gmail.com>
2022-04-08 20:11:40 +0500

    change dependencies

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
 const express = require('express');
-const PORT = 3000;
+const router = require('./routers');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(express.json());
+app.use('/api', router);
 
 const start = async() => {
     try {
-        app.listen(PORT, () => console.log('Server start has been listen'))
+        app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`))
     } catch(err) {
         console.log(err);
     }
